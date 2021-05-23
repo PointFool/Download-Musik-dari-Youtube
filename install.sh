e31545370f576a2aecb84eda61158b2da76ad213
ExWitch <83410728+PointFool@users.noreply.github.com>
2021-05-23 20:58:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 echo "install";
 echo ""
 echo "WAIT GAN";
-cd /data/data/com.termux/files/home/Download-Musik-dari-Youtube-Termux/ && mv yt.sh /data/data/com.termux/files/usr/bin/yt
+mv yt.sh /data/data/com.termux/files/usr/bin/yt
 chmod +x /data/data/com.termux/files/home/usr/bin/yt
 echo "Done";
